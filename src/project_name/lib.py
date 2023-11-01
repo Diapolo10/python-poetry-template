eb2df2995612e62e86db3f99b646ab7e90c33d73
Lari Liuhamo <lari.liuhamo@gmail.com>
2023-11-01 22:41:59 +0200

    [PATCH] Coverage reports now generate

---
+++ src/project_name/lib.py
@@ -0,0 +1,5 @@
+"""Code."""
+
+def adder(first: int, second: int) -> int:
+    """Lorem Ipsum dolor sit amet."""
+    return first + second
--- tests/test_default.py
+++ tests/test_default.py
@@ -1,7 +1,14 @@
 """Stub unit test file."""
 
+from project_name.lib import adder
+
 
 def test_stub() -> None:
     """Lorem Ipsum."""
     assert True
     assert not False
+
+
+def test_adder():
+    """Test the adder function."""
+    assert adder(3, 4) == 7  # noqa: PLR2004
