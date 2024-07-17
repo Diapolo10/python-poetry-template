b833e086d3b3287e1a59b73f5c1b330f0651808e
Lari Liuhamo <lari.liuhamo@gmail.com>
2024-07-17 15:31:47 +0300

    [PATCH] Progress

--- src/project_name/config.py
+++ src/project_name/config.py
@@ -2,6 +2,7 @@
 
 import os
 from importlib import resources as pkg_resources
+from pathlib import Path
 
 from dotenv import load_dotenv
 
@@ -9,8 +10,9 @@ import project_name
 
 load_dotenv()
 
+PACKAGE_NAME = "project_name"
+
 with pkg_resources.as_file(pkg_resources.files(project_name)) as package_dir:
     DEFAULT_CONFIG_FILE_PATH = package_dir / 'logger_config.toml'
-    PYPROJECT_TOML_FILE_PATH = package_dir.parent / 'pyproject.toml'
 
-LOGGER_CONFIG_FILE = os.environ.get("PROJECT_NAME_LOGGER_CONFIG_FILE", DEFAULT_CONFIG_FILE_PATH)
+LOGGER_CONFIG_FILE = Path(os.environ.get("PROJECT_NAME_LOGGER_CONFIG_FILE", DEFAULT_CONFIG_FILE_PATH))
--- src/project_name/logger.py
+++ src/project_name/logger.py
@@ -1,5 +1,7 @@
 """Configure the root logger."""
 
+from __future__ import annotations
+
 import atexit
 import datetime as dt
 import json
@@ -7,22 +9,29 @@ import logging
 import logging.config
 import logging.handlers
 from enum import Enum, auto
-from typing import NotRequired, TypedDict, override
+from pathlib import Path
+from typing import Any, NotRequired, TypedDict, override
 
 try:
     import tomllib
 except ImportError:
-    import tomli as tomllib
+    import tomli as tomllib  # type: ignore[import-not-found, no-redef]
 
-from project_name.config import LOGGER_CONFIG_FILE, PYPROJECT_TOML_FILE_PATH
+from project_name.config import LOGGER_CONFIG_FILE, PACKAGE_NAME
 
-ROOT_LOGGER_NAME = tomllib.loads(PYPROJECT_TOML_FILE_PATH.read_text(encoding='utf-8'))['tool']['poetry']['name']
+ROOT_LOGGER_NAME = PACKAGE_NAME
 
 __all__ = ('ROOT_LOGGER_NAME', 'setup_logging')
 
 
 class RecordAttrs(str, Enum):
     """Log record attributes."""
+
+    @override
+    @staticmethod
+    def _generate_next_value_(name: str, start: int, count: int, last_values: list) -> str:
+        """Create enum values from the names automatically."""
+        return name
 
     args = auto()
     asctime = auto()
@@ -38,7 +47,7 @@ class RecordAttrs(str, Enum):
     msecs = auto()
     message = auto()
     msg = auto()
-    name = auto()
+    name: str = "name"
     pathname = auto()
     process = auto()
     processName = auto()
@@ -47,12 +56,6 @@ class RecordAttrs(str, Enum):
     thread = auto()
     threadName = auto()
     taskName = auto()
-
-    @override
-    @staticmethod
-    def _generate_next_value_(name: str, start: int, count: int, last_values: list) -> str:
-        """Create enum values from the names automatically."""
-        return name
 
 
 class FormatKeys(TypedDict):
@@ -79,14 +82,13 @@ class CustomQueueHandler(logging.handlers.QueueHandler):
     """Custom queue handler."""
 
     @override
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, *args: Any, **kwargs: dict[str, Any]) -> None:
         queue_handler = logging.getHandlerByName("queue_handler")
         if queue_handler is None:
             super().__init__(*args, **kwargs)
         else:
             queue_handler.listener.start()
             atexit.register(queue_handler.listener.stop)
-
 
 
 class JSONLogFormatter(logging.Formatter):
@@ -108,7 +110,7 @@ class JSONLogFormatter(logging.Formatter):
             'timestamp': dt.datetime.fromtimestamp(
                 record.created,
                 tz=dt.timezone.utc,
-            ).isoformat(),
+            ),
         }
 
         if record.exc_info is not None:
@@ -135,5 +137,12 @@ class JSONLogFormatter(logging.Formatter):
 
 def setup_logging() -> None:
     """Set up logging."""
-    logging_config = json.loads(LOGGER_CONFIG_FILE.read_text(encoding='utf-8'))
+    (Path.cwd() / 'logs').mkdir(exist_ok=True)
+    logger_data = LOGGER_CONFIG_FILE.read_text(encoding='utf-8')
+    logging_config = tomllib.loads(logger_data)
     logging.config.dictConfig(logging_config)
+
+    queue_handler = logging.getHandlerByName("queue_handler")
+    if queue_handler is not None:
+        queue_handler.listener.start()  # type: ignore[attr-defined]
+        atexit.register(queue_handler.listener.stop)  # type: ignore[attr-defined]
