d76f0381e2941f0a54d51ac041faee3722414cc3
Lari Liuhamo <lari.liuhamo@gmail.com>
2024-07-17 15:31:47 +0300

    [PATCH] Progress

--- src/project_name/main.py
+++ src/project_name/main.py
@@ -2,15 +2,16 @@
 
 import logging
 
-from project_name.logger import ROOT_LOGGER_NAME, setup_logging
+from project_name.logger import ROOT_LOGGER_NAME, setup_logging, CustomQueueHandler
 
 logger = logging.getLogger(ROOT_LOGGER_NAME)
 
 
-def main():
-    """Main program."""
+def main() -> None:
+    """Lorem Ipsum."""
     setup_logging()
-    # logging.basicConfig(level=logging.INFO)
+    # logger.addHandler(CustomQueueHandler(level=logging.INFO))
+    # logging.basicConfig(level=logging.INFO)  # noqa: ERA001
 
     logger.debug("debug message", extra={'foo': 'hello'})
     logger.info("info message")
@@ -19,9 +20,9 @@ def main():
     logger.critical("critical message")
 
     try:
-        1337 / 0
+        _ = 1337 / 0
     except ZeroDivisionError:
-        logger.error("exception message")
+        logger.exception("exception message")
 
 
 if __name__ == '__main__':
