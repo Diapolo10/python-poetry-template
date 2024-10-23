c091e474782d2f746fc451611a7de3ca7f7af723
Lari Liuhamo <lari.liuhamo@gmail.com>
2024-10-23 23:44:50 +0300

    [PATCH] Fixed logging

--- src/project_name/logger.py
+++ src/project_name/logger.py
@@ -58,11 +58,34 @@ class RecordAttrs(str, Enum):
     taskName = auto()
 
 
+class ColouredFormatter(logging.Formatter):
+    """Coloured log formatter."""
+
+    @override
+    def format(self, record: logging.LogRecord) -> str:
+        """Format the log record."""
+        log_level_colours = {
+            logging.CRITICAL: '\033[31;1;40m',  # Red, bold
+            logging.ERROR: '\033[31;40m',       # Red
+            logging.WARNING: '\033[33;40m',     # Yellow
+            logging.INFO: '\033[32;40m',        # Green
+            logging.DEBUG: '\033[36;40m',       # Cyan
+        }
+        reset = '\033[0m'
+
+        record.msg = f"{log_level_colours.get(record.levelno, reset)}{record.msg}{reset}"
+        record.levelname = (
+            f"{log_level_colours.get(record.levelno, reset)}{record.levelname:^8}{reset}"
+        )
+
+        return super().format(record)
+
+
 class FormatKeys(TypedDict):
     """Log format keys."""
 
-    message: str
-    timestamp: dt.datetime
+    message: NotRequired[str]
+    timestamp: NotRequired[dt.datetime]
     level: NotRequired[str]
     logger: NotRequired[str]
     module: NotRequired[str]
@@ -82,13 +105,10 @@ class CustomQueueHandler(logging.handlers.QueueHandler):
     """Custom queue handler."""
 
     @override
-    def __init__(self, *args: Any, **kwargs: dict[str, Any]) -> None:
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
         queue_handler = logging.getHandlerByName("queue_handler")
         if queue_handler is None:
-            super().__init__(*args, **kwargs)
-        else:
-            queue_handler.listener.start()
-            atexit.register(queue_handler.listener.stop)
+            super().__init__(*args, **kwargs)  # type: ignore[arg-type]
 
 
 class JSONLogFormatter(logging.Formatter):
@@ -97,7 +117,11 @@ class JSONLogFormatter(logging.Formatter):
     @override
     def __init__(self, *, fmt_keys: FormatKeys | None = None) -> None:
         super().__init__()
-        self.fmt_keys = fmt_keys if fmt_keys is not None else {}
+        self.fmt_keys = (
+            fmt_keys
+            if fmt_keys is not None
+            else FormatKeys()
+        )
 
     @override
     def format(self, record: logging.LogRecord) -> str:
@@ -119,10 +143,10 @@ class JSONLogFormatter(logging.Formatter):
         if record.stack_info is not None:
             required_fields['stack_info'] = self.formatStack(record.stack_info)
 
-        message: LogDict = {
+        message: LogDict = {  # type: ignore[assignment]
             key: msg_val
-            if (msg_val := required_fields.pop(val, None)) is not None
-            else getattr(record, val)
+            if (msg_val := required_fields.pop(val, None)) is not None  # type: ignore[misc, typeddict-item]
+            else getattr(record, val)  # type: ignore[call-overload]
             for key, val in self.fmt_keys.items()
         }
 
@@ -130,7 +154,7 @@ class JSONLogFormatter(logging.Formatter):
 
         for key, val in record.__dict__.items():
             if key not in RecordAttrs:
-                message[key] = val
+                message[key] = val  # type: ignore[literal-required]
 
         return message
 
--- src/project_name/main.py
+++ src/project_name/main.py
@@ -2,7 +2,7 @@
 
 import logging
 
-from project_name.logger import ROOT_LOGGER_NAME, setup_logging, CustomQueueHandler
+from project_name.logger import ROOT_LOGGER_NAME, setup_logging
 
 logger = logging.getLogger(ROOT_LOGGER_NAME)
 
@@ -10,8 +10,6 @@ logger = logging.getLogger(ROOT_LOGGER_NAME)
 def main() -> None:
     """Lorem Ipsum."""
     setup_logging()
-    # logger.addHandler(CustomQueueHandler(level=logging.INFO))
-    # logging.basicConfig(level=logging.INFO)  # noqa: ERA001
 
     logger.debug("debug message", extra={'foo': 'hello'})
     logger.info("info message")
